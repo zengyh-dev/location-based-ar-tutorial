09babab7c4b4e252d51589201b4186e721c21def
Leo <2493765289@qq.com>
2022-02-13 21:36:49 +0800

    '位置设置'

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
     {
       name: 'Magnemite',
       location: {
-        lat: 44.496470,
-        lng: 11.320180,
+        lat: 84.496470,
+        lng: 31.320180,
       }
     },
   ];
